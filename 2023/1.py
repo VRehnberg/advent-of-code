1426bbc5545be2b4a91aa93802c210d2c79191dd
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2023-12-01 08:15:15 +0100

    Add task 1.2

--- 2023/1.py
+++ 2023/1.py
@@ -1,15 +1,34 @@
-import re
-
+written_digits = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
 
 with open("1.in") as f:
     calibration_sum = 0
-    re_digit = re.compile("\D+", flags=re.ASCII)
     for line in f:
-        # Remove non digit characters
-        digits = re_digit.sub("", line)
+        # Get digits in string
+        digits = ""
+        for i, c in enumerate(line):
+            # String representations of digits
+            if c.isdigit():
+                digits += c
+
+            # Look for written digit starting from this point in the line
+            for digit, written_digit in enumerate(written_digits):
+                if line[i:].startswith(written_digit):
+                    digits += f"{digit}"
 
         # Create calibration value and then add to sum
         calibration_value = int(digits[0] + digits[-1])
+        assert calibration_value > 9 and calibration_value % 10 != 0
         calibration_sum += calibration_value
 
 print(calibration_sum)
