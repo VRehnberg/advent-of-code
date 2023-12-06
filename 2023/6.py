c8bd2323f0dd1b3425e038d56f7e26ea64df811a
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2023-12-06 16:09:48 +0100

    Day 6 task 2

--- 2023/6.py
+++ 2023/6.py
@@ -2,14 +2,11 @@ import numpy as np
 
 
 with open("6.in") as f:
-    times = tuple(map(int, next(f).split(":")[-1].strip().split()))
-    record_distances = tuple(map(int, next(f).split(":")[-1].strip().split()))
+    time = int("".join(next(f).split(":")[-1].split()))
+    record_distance = int("".join(next(f).split(":")[-1].split()))
 
-    n_possible_wins = []
-    for time, record_distance in zip(times, record_distances):
-        speed = np.arange(0, time + 1)
-        run_time = time - speed
-        n_possible_wins.append(np.count_nonzero(run_time * speed > record_distance))
+speed = np.arange(0, time + 1)
+run_time = time - speed
+n_possible_wins = np.count_nonzero(run_time * speed > record_distance)
 
 print(n_possible_wins)
-print(np.prod(n_possible_wins))
