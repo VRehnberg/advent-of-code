484ca3130bd615f0ccc1e6aa7a123eb04a4ba459
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2023-12-08 12:30:35 +0100

    Idea for speed-up

--- 2023/8.py
+++ 2023/8.py
@@ -1,6 +1,6 @@
 import re
 from itertools import cycle
-from typing import IO
+from typing import IO, Iterable
 
 
 re_nodes = re.compile(r"^([0-9A-Z]+) = [(]([0-9A-Z]+), ([0-9A-Z]+)[)]$")
@@ -32,15 +32,53 @@ def task1(instructions, nodes):
     print(i_step)
 
 
+class Loop:
+    def __init__(self, initial_node, instructions, nodes):
+        self.initial_node = initial_node
+        location = self.initial_node
+        loop_found = False
+        visited: list[tuple(int, str)] = []
+        for i_step, instruct in cycle(enumerate(instructions)):
+            if (i_step, location) in visited:
+                break
+            location = nodes[location][instruct]
+            visited.append((i_step, location))
+
+        self.offset = i_step
+        _, visited_nodes = zip(*visited)
+        self.loop = tuple(visited_nodes[i_step:])
+
+    def __len__(self):
+        return len(self.loop)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.initial_node})"
+
+
 def task2(instructions, nodes):
-    locations = [node for node in nodes if node.endswith("A")]
+    # Find loops
+    loops = [
+        Loop(node, instructions, nodes)
+        for node in nodes
+        if node.endswith("A")
+    ]
+    print(loops)
+    locations = [loop.initial_node for loop in loops]
     print(locations)
-    for i_step, instruct in enumerate(cycle(instructions)):
+
+    # Check for stop before all loops have started
+    offset = max(loop.offset for loop in loops)
+    print(offset)
+    print([len(loop) for loop in loops])
+    for i_step, instruct in zip(range(offset), cycle(instructions)):
         if all(loc.endswith("Z") for loc in locations):
-            break
+            print(i_step + 1)
+            return
         locations = [nodes[loc][instruct] for loc in locations]
-        #assert not any((loc, loc) == nodes[loc] for loc in locations), "Stuck"
-    print(i_step)
+
+    print("TODO least_common_stop based on loops")
+    # Possible offsets are loop.offset + [i for i, n in loop if n.endswith("Z")]
+    # Check all LCM with offset for each
 
 
 def main():
