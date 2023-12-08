6229b9ba70ee8b5fdb2062476cc0ee2e57259d78
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2023-12-08 09:25:53 +0100

    Day 8 slow task 2

--- 2023/8.py
+++ 2023/8.py
@@ -1,28 +1,55 @@
 import re
 from itertools import cycle
+from typing import IO
 
 
-re_nodes = re.compile(r"^([A-Z]+) = [(]([A-Z]+), ([A-Z]+)[)]$")
+re_nodes = re.compile(r"^([0-9A-Z]+) = [(]([0-9A-Z]+), ([0-9A-Z]+)[)]$")
 
 
-with open("8.in") as f:
-    instructions = next(f).strip()
-    assert next(f).strip() == ""
+def parse(file: IO) -> tuple[str, dict[str, tuple[str, str]]]:
+        instruct2index = {
+            "L": 0,
+            "R": 1,
+        }
+        instructions = [instruct2index[instruct] for instruct in next(file).strip()]
+        assert next(file).strip() == ""
 
-    nodes: dict[str, tuple[str, str]] = {}
-    for line in f:
-        match = re_nodes.match(line.strip())
-        nodes[match.group(1)] = (match.group(2), match.group(3))
+        nodes: dict[str, tuple[str, str]] = {}
+        for line in file:
+            match = re_nodes.match(line.strip())
+            nodes[match.group(1)] = (match.group(2), match.group(3))
+
+        return instructions, nodes
 
 
-instruct2index = {
-    "L": 0,
-    "R": 1,
-}
-location = "AAA"
-for i_step, instruct in enumerate(cycle(instructions)):
-    if location == "ZZZ":
-        break
-    location = nodes[location][instruct2index[instruct]]
+def task1(instructions, nodes):
+    location = "AAA"
+    for i_step, instruct in enumerate(cycle(instructions)):
+        if location == "ZZZ":
+            break
+        location = nodes[location][instruct]
 
-print(i_step)
+    print(i_step)
+
+
+def task2(instructions, nodes):
+    locations = [node for node in nodes if node.endswith("A")]
+    print(locations)
+    for i_step, instruct in enumerate(cycle(instructions)):
+        if all(loc.endswith("Z") for loc in locations):
+            break
+        locations = [nodes[loc][instruct] for loc in locations]
+        #assert not any((loc, loc) == nodes[loc] for loc in locations), "Stuck"
+    print(i_step)
+
+
+def main():
+    with open("8.in") as f:
+        instructions, nodes = parse(f)
+
+    task1(instructions, nodes)
+    task2(instructions, nodes)
+
+
+if __name__ == "__main__":
+    main()
