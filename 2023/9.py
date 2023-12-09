bd9027410bec6d23490eaaa26f3a60fedb37e9e2
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2023-12-09 07:01:39 +0100

    Day 2 task 2

--- 2023/9.py
+++ 2023/9.py
@@ -7,18 +7,26 @@ with open("9.in") as f:
         for line in f
     ]
 
-# Task 1
-next_val_sum = 0
+# Task 2
+prev_val_sum = 0
 for serie in series:
+    #print("S", serie)
     diffs = np.diff(serie)
-    increments = [diffs[-1]]
+    #print("D", diffs)
+    decrements = [diffs[0]]
     while np.count_nonzero(diffs) != 0:
         diffs = np.diff(diffs)
-        increments.append(diffs[-1])
+        decrements.append(diffs[0])
+        #print("D", diffs)
+    #print(decrements)
 
-    print(increments)
-    next_val = serie[-1] + sum(increments)
-    next_val_sum += next_val
-    print(next_val)
+    prev_val = serie[0]
+    meta_decrement = 0
+    for i, decrement in enumerate(decrements[::-1]):
+        meta_decrement = decrement - meta_decrement
+        #print(i, meta_decrement)
+    prev_val -= meta_decrement
+    print(prev_val)
+    prev_val_sum += prev_val
 
-print(next_val_sum)
+print(prev_val_sum)
